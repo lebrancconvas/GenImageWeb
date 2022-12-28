f4c9e6e0b841fc7d48ff6710480686db32446a75
lebrancconvas <lebrancconvas@gmail.com>
2022-12-28 22:16:37 +0700

    ✨ feat: update

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -12,6 +12,17 @@ const Home: NextPage = () => {
           <h1>Gen Image Website</h1> 
         </div>
       </header>
+      <section id="input-section">
+        <div>
+          <h2>Typing your prompt...</h2> 
+        </div>
+        <div>
+          <input type="text" name="promput" className="block w-full bg-grey-100 border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500" placeholder="Insert your prompt..." />  
+          <div>
+            <button className="btn btn-primary">Generate</button> 
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
